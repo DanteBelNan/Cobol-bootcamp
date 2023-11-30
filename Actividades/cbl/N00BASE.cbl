a7057f84e25a9f8ce8f1ac973272eb5567bbfc72
Dante Beltran Nanziot <dantebelnan@gmail.com>
2023-11-29 23:38:58 -0300

    Archivo base hecho

--- Actividades/cbl/N00BASE.cbl
+++ Actividades/cbl/N00BASE.cbl
@@ -1,37 +1,42 @@
-       IDENTIFICATION DIVISION.
+       IDENTIFICATION DIVISION. 
        PROGRAM-ID. N00BASE.
-      
-       DATA DIVISION.
-       FILE SECTION.
-       FD ARCHIVO-ENTRADA.
-       01 REGISTRO-ENTRADA.
-       05 NUMERO PIC 9(2).
-      
-       WORKING-STORAGE SECTION.
-       01 CONTADOR PIC 9(2).
-      
+       
+       DATA DIVISION. 
+       FILE SECTION. 
+       FD ARCHIVOTEST.
+          01 REG-TEST.
+              05 NUMERO .
+       WORKING-STORAGE SECTION. 
+          01 CONTADOR PIC 9(3) VALUE 1.
        PROCEDURE DIVISION.
-      
-       MAIN-PROCEDURE.
-      
-       OPEN INPUT ARCHIVO-ENTRADA.
-      
-       SET CONTADOR TO 0.
-      
-       LEER ARCHIVO-ENTRADA INTO REGISTRO-ENTRADA.
-      
-       WHILE NOT EOF(ARCHIVO-ENTRADA)
-      
-       DO.
-      
-       INCREASE CONTADOR BY 1.
-      
-       DISPLAY "El número es: " NUMERO.
-      
-       LEER ARCHIVO-ENTRADA INTO REGISTRO-ENTRADA.
-      
-       END-WHILE.
-      
-       CLOSE ARCHIVO-ENTRADA.
-      
-       STOP RUN.
+           PERFORM 1000-INICIO
+           THRU 1000-INICIO-EXIT
+
+           PERFORM 2000-PROCESO
+           THRU 2000-PROCESO-EXIT  
+
+           PERFORM 3000-FIN
+           THRU 3000-FIN-EXIT
+           STOP RUN.
+
+       1000-INICIO.
+           DISPLAY 'Entro al inicio'
+           OPEN OUTPUT ARCHIVOTEST.
+       1000-INICIO-EXIT.
+           EXIT.
+       2000-PROCESO.
+           DISPLAY 'Entro al proceso'
+           PERFORM UNTIL CONTADOR > 5
+             DISPLAY CONTADOR
+             MOVE CONTADOR TO NUMERO 
+             WRITE REG-TEST FROM NUMERO 
+             ADD 1 TO CONTADOR 
+             END-PERFORM.
+       2000-PROCESO-EXIT.
+           EXIT.
+       3000-FIN.
+           DISPLAY 'Entro al fin'
+           CLOSE ARCHIVOTEST
+           STOP RUN.
+       3000-FIN-EXIT.
+           EXIT.

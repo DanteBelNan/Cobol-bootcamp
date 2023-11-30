3c93e0c868bf9fbffc72ced6f21d76bb1297c3d1
Dante Beltran Nanziot <dantebelnan@gmail.com>
2023-11-29 23:49:25 -0300

    Archivo base completamente hecho

--- Actividades/cbl/N00BASE.cbl
+++ Actividades/cbl/N00BASE.cbl
@@ -2,10 +2,6 @@
        PROGRAM-ID. N00BASE.
        
        DATA DIVISION. 
-       FILE SECTION. 
-       FD ARCHIVOTEST.
-          01 REG-TEST.
-              05 NUMERO .
        WORKING-STORAGE SECTION. 
           01 CONTADOR PIC 9(3) VALUE 1.
        PROCEDURE DIVISION.
@@ -20,23 +16,17 @@
            STOP RUN.
 
        1000-INICIO.
-           DISPLAY 'Entro al inicio'
-           OPEN OUTPUT ARCHIVOTEST.
+           DISPLAY "Entro al inicio"
+           .
        1000-INICIO-EXIT.
            EXIT.
        2000-PROCESO.
-           DISPLAY 'Entro al proceso'
-           PERFORM UNTIL CONTADOR > 5
-             DISPLAY CONTADOR
-             MOVE CONTADOR TO NUMERO 
-             WRITE REG-TEST FROM NUMERO 
-             ADD 1 TO CONTADOR 
-             END-PERFORM.
+           DISPLAY "Entro al proceso"
+           .
        2000-PROCESO-EXIT.
            EXIT.
        3000-FIN.
-           DISPLAY 'Entro al fin'
-           CLOSE ARCHIVOTEST
-           STOP RUN.
+           DISPLAY "Entro al fin"
+           .
        3000-FIN-EXIT.
            EXIT.
